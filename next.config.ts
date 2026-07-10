f81c823a2c15cae883802416f17e06d718e702f7
goncalo300 <diananeste22@gmail.com>
2026-07-10 13:03:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedHosts: [
-  "goncalographics.ru",
-  "www.goncalographics.ru",
-  "localhost",
-  "127.0.0.1",
-],
 };
 
 export default nextConfig;
